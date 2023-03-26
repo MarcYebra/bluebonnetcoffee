de311d78e17ede1cd53518b9c214fe86172604de
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2023-03-26 11:27:42 -0500

    added content

--- app/javascript/react/components/About.js
+++ app/javascript/react/components/About.js
@@ -4,14 +4,26 @@ import React from 'react';
 const About = (props) => {
 
   return (
-    <div className='font-prohibition'>
-      <div className='our-story-flex'>
-        <h2 >Our Story</h2>
-          <p>Our passion for creating coffee comes from the pride of being a Texan. 
-            We dedicate our work to all of the hardworking Texans who 
-          </p>
+    <div>
+      <div className='about-top-image'></div>
+      <div className='about-container font-prohibition'>
+          <h2>Our Story</h2>
+            <h4>It All Started in The Heart Of Texas</h4>
+            <p>Howdy! Bluebonnet Coffee was built on the idea of cultivating a tribe of individuals who identify themselves as 
+              hardworking, brave, resilient, and friendly. Whichever road led you to us, it doesn’t matter. Our belief at Bluebonnet
+              is that everyone be treated with the same level of respect and kindness. Our goal is for every customer to leave
+              satisfied and inspired to face the world head on!
+            </p>
+
+          <h2>Why Bluebonnet Coffee?</h2>
+            <p>Being a Texan isn’t about throwing on cowboy boots and saying “yee-haw,” it’s a way of life. Just like bluebonnets, 
+              Texans are vibrant, work best in heat, and hard to ignore because of how darn beautiful we are! <br></br>
+              Coffee shops are more than places to sit and drink, they’re ways of expressing yourself. Bluebonnet Coffee hopes to 
+              fill your cup with love, passion, and a little bit of fire to light up your day. We pride ourselves in our work ethic, so whether 
+              you’re a student needing a place to study, an entrepreneur needing a place to meet with investors, or you’ve got family 
+              visiting from Boston, Bluebonnet Coffee is here to serve your needs. 
+            </p>
       </div>
-        <div className='coffee-beans-hands'></div>
     </div>
   )
 }
